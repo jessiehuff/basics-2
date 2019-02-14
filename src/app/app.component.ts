06d82b5b4310cd240e4e42a63807105b987f872c
Jessie Huff <huffjd1@gmail.com>
2019-02-14 14:56:45 -0500

    Created form input with databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent { 
+  title = 'Our app!';
+  username = ''; 
+  allowNewUsername = false; 
+  usernameStatus = 'No username was created!'; 
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewUsername = true; 
+    }, 2000); 
+  }
+
+  ngOnInit() {
+  }
+  
+  onCreateUsername(){
+    this.usernameStatus = 'Username was created! Name is ' + this.username; 
+  }
+
+  onUpdateUsername(event: Event){
+    this.username = (<HTMLInputElement>event.target).value; 
+  }
 }
